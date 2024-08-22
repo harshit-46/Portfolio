7180147c1d06fcd5b284a6fd42d54391bbec88e6
Harshit Gupta <iamharshit999@gmail.com>
2024-08-23 00:17:25 +0530

    update

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import githubLogo from "../assets/icons/github.png";
+import AppLogo from "../assets/icons/AppLogo.png";
 
 const Header = () => {
     return (
         <div className="bg-black">
             <nav className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-4 flex items-center justify-between">
-                <Link to="/" className="flex-shrink-0" aria-current="page"><img src="../assets/icons/AppLogo.png" height="80" width="80" className="ml-2 sm:ml-0" /></Link>
+                <Link to="/" className="flex-shrink-0" aria-current="page"><img src={AppLogo} height="80" width="80" className="ml-2 sm:ml-0" /></Link>
                 <div className="hidden sm:block">
                     <ul className="flex space-x-4">
                         <li><Link to="/home" className="text-white px-3 py-2 rounded hover:bg-gray-700">Home</Link></li>
@@ -14,7 +16,7 @@ const Header = () => {
                         <li><Link to="/contact" className="text-white px-3 py-2 rounded hover:bg-gray-700">Contact</Link></li>
                     </ul>
                 </div>
-                <a href="https://github.com/harshit-46" target="_blank" rel="noreferrer"><img src="../assets/icons/ghmain.PNG" alt="" height="32" width="32" className="ml-4 sm:ml-2" /></a>
+                <a href="https://github.com/harshit-46" target="_blank" rel="noreferrer"><img src={githubLogo} alt="" height="32" width="32" className="ml-4 sm:ml-2" /></a>
             </nav>
         </div>
     );
