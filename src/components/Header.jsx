a8059025ba078c10fd666d6f1f3d0a5466caa06c
Harshit Gupta <iamharshit999@gmail.com>
2024-09-04 00:09:01 +0530

    update

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,13 +2,14 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import githubLogo from "../assets/icons/github.png";
 import AppLogo from "../assets/icons/AppLogo.png";
+import '../assets/CSS/header.css';
 
 const Header = () => {
     return (
         <div className="bg-black border-b border-stone-600 sticky top-0 z-50"> {/* bg-opacity-60 shadow-md */}
-            <nav className="max-w-7xl mx-auto py-2 flex items-center justify-between h-14">
+            <nav id="navbar" className="max-w-7xl mx-auto py-2 flex items-center justify-between h-14">
                 <Link to="/" className="flex-shrink-0" aria-current="page"><img src={AppLogo} height="80" width="80" className="ml-2 sm:ml-0" /></Link>
-                <div className="hidden sm:block">
+                <div id="nav-head" className="hidden sm:block">
                     <ul className="flex space-x-4">
                         <li><Link to="/home" className="text-xl font-normal text-stone-400 px-3 py-2 rounded hover:text-white">Home</Link></li>
                         <li><Link to="/skills" className="text-xl font-normal text-stone-400 px-3 py-2 rounded hover:text-white">Skills</Link></li>
