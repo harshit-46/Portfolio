f87e588e46617115aa605fe444bc112f30125085
Harshit Gupta <iamharshit999@gmail.com>
2024-09-07 00:10:19 +0530

    update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,7 +7,7 @@ const Footer = () => {
         <div className='bg-black text-white h-72 border-t border-stone-800 '>
             <img src={logo} height="90px" width="90px" className='ml-32 mt-16'/>
             <div id="foot-para" className='text-stone-500'>Developed by <a href="https://www.linkedin.com/in/harshithere/" id="foot-link" target='_blank'>Harshit Gupta</a></div>
-            <div>
+            <div id="links" className='border-2 border-red-500'>
                 <a href="https://drive.google.com/file/d/1_6PuQCpMIhatcA7hvkGFp-wTSXBNbBO0/view" target='_blank'>Resume</a>
                 <a href="https://www.linkedin.com/in/harshithere/" target='_blank'>Linkedin</a>
                 <a href="https://www.instagram.com/_harshit.25/" target='_blank'>Instagram</a>
