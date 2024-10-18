b2d2a3588d637a4b2c6a232bd235ac41a0f951a5
Harshit Gupta <iamharshit999@gmail.com>
2024-10-19 00:39:02 +0530

    update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ import '../assets/CSS/footer.css'
 
 const Footer = () => {
     return (
-        <div className='bg-black text-white h-72 border-t border-stone-800 '>
+        <div className='bg-black text-white h-80 border-t border-stone-800 '>
             <img src={logo} height="90px" width="90px" className='ml-32 mt-16'/>
             <div id="foot-para" className='text-stone-500'>Developed by <a href="https://www.linkedin.com/in/harshithere/" id="foot-link" target='_blank'>Harshit Gupta</a></div>
             <div id="links" className='border-2 border-red-500'>
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,14 +1,12 @@
 import React from 'react';
-//import { GridBackgroundProjects } from './test/grid-projects';
-//import Footer from './Footer';
+import Footer from './Footer';
 import { TracingBeamDemo } from './test/tracebeam';
 
 const Projects = () => {
     return (
-        <div>
-            {/*<GridBackgroundProjects/>*/}
+        <div className='bg-black'>
             <TracingBeamDemo/>
-            {/*<Footer/>*/}
+            <Footer/>
         </div>
     )
 }
