a5e3533ebf47a974bdfdb1abbe647707624cf6be
Harshit Gupta <iamharshit999@gmail.com>
2025-01-04 18:37:54 +0530

    update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,22 +4,23 @@ import '../assets/CSS/footer.css'
 
 const Footer = () => {
     return (
-    <div className='h-80 bg-black text-white flex'>
+    <div className='h-80 bg-[#1C1C1C] text-white flex'>
         <div className='border-t w-1/3 border-stone-800'>
             <img src={logo} height="90px" width="90px" className='ml-32 mt-16'/>
-            <div id="foot-para" className='text-stone-500'>
+            <div id="foot-para" className='text-stone-400'>
                 Developed by
                 <a 
                 href="https://www.linkedin.com/in/harshithere/" 
-                className='text-blue-500 hover:text-white transition duration-200'
+                className='text-[#52a8ff] hover:text-purple-400 transition duration-200'
                 target='_blank'
                 rel="noopener noreferrer">
                 &nbsp;Harshit Gupta
                 </a>
+                <p className='copyright pt-20'>Copyright &#169; 2025. All rights reserved.</p>
             </div>
         </div>
         <div className='w-2/3 border-t border-stone-800 pl-12'>
-            hello
+            
         </div>
     </div>
     )
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,21 +1,40 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import githubLogo from "../assets/icons/github.png";
 import AppLogo from "../assets/icons/AppLogo.png";
-import '../assets/CSS/header.css';
 
 const Header = () => {
     return (
         <header className="navbar-main">
             <nav className="navbar border-b border-stone-800">
-                <Link to="/" className="applogo" alt="App logo" aria-current="page"><img src={AppLogo} height="80" width="80"/></Link>
+                <NavLink to="/" className="applogo" alt="App logo" aria-current="page"><img src={AppLogo} height="80" width="80"/></NavLink>
                 <div className="nav-head">
                     <ul className="links">
-                        <li><Link to="/home" className='text-white hover:text-blue-500 transition duration-200'>Home</Link></li>
-                        <li><Link to="/skills" className='text-white hover:text-blue-500 transition duration-200'>Skills</Link></li>
-                        <li><Link to="/education" className='text-white hover:text-blue-500 transition duration-200'>Education</Link></li>
-                        <li><Link to="/projects" className='text-white hover:text-blue-500 transition duration-200'>Projects</Link></li>
-                        <li><Link to="/contact" className='text-white hover:text-blue-500 transition duration-200'>Contact</Link></li>
+                        <li><NavLink to="/home" className={({ isActive }) =>
+                                    isActive
+                                        ? 'text-[#52a8ff] transition duration-200'
+                                        : 'text-gray-300 hover:text-white transition duration-200'
+                                }>Home</NavLink></li>
+                        <li><NavLink to="/skills" className={({ isActive }) =>
+                                    isActive
+                                        ? 'text-[#52a8ff] transition duration-200'
+                                        : 'text-gray-300 hover:text-white transition duration-200'
+                                }>Skills</NavLink></li>
+                        <li><NavLink to="/education" className={({ isActive }) =>
+                                    isActive
+                                        ? 'text-[#52a8ff] transition duration-200'
+                                        : 'text-gray-300 hover:text-white transition duration-200'
+                                }>Education</NavLink></li>
+                        <li><NavLink to="/projects" className={({ isActive }) =>
+                                    isActive
+                                        ? 'text-[#52a8ff] transition duration-200'
+                                        : 'text-gray-300 hover:text-white transition duration-200'
+                                }>Projects</NavLink></li>
+                        <li><NavLink to="/contact" className={({ isActive }) =>
+                                    isActive
+                                        ? 'text-[#52a8ff] transition duration-200'
+                                        : 'text-gray-300 hover:text-white transition duration-200'
+                                }>Contact</NavLink></li>
                     </ul>
                 </div>
                 <a href="https://github.com/harshit-46" alt="Github logo" target="_blank" rel="noopener noreferrer"><img src={githubLogo} height="32" width="32" className="githubLogo" /></a>
