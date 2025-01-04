8a32b33ac3fb34039c798ab442e6f2b1569efb5b
Harshit Gupta <iamharshit999@gmail.com>
2025-01-04 22:50:47 +0530

    footer links

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import logo from '../assets/icons/AppLogo.png';
 import '../assets/CSS/footer.css'
 
 const Footer = () => {
     return (
-    <div className='h-80 bg-[#1C1C1C] text-white flex'>
-        <div className='border-t w-1/3 border-stone-800'>
+    <div className='h-96 bg-[#1C1C1C] text-white flex flex-col md:flex-row'>
+        <div className='border-t w-full md:w-1/3 border-stone-800'>
             <img src={logo} height="90px" width="90px" className='ml-32 mt-16'/>
             <div id="foot-para" className='text-stone-400'>
                 Developed by
@@ -13,14 +14,53 @@ const Footer = () => {
                 href="https://www.linkedin.com/in/harshithere/" 
                 className='text-[#52a8ff] hover:text-purple-400 transition duration-200'
                 target='_blank'
-                rel="noopener noreferrer">
+                rel="noopener noreferrer"
+                aria-label="Visit Harshit Gupta's LinkedIn Profile">
                 &nbsp;Harshit Gupta
                 </a>
-                <p className='copyright pt-20'>Copyright &#169; 2025. All rights reserved.</p>
+                <p className='copyright pt-36 text-[#A3A3A3]'>Copyright &#169; 2025. All rights reserved.</p>
             </div>
         </div>
-        <div className='w-2/3 border-t border-stone-800 pl-12'>
-            
+        <div className='footer-links w-full md:w-2/3 border-t border-stone-800'>
+            <div className='flex flex-col items-start float-left mt-14 pl-44'>
+                <h4 id="Main-section" className="mb-4 text-stone-500">USEFUL LINKS</h4>
+                <Link to="/home" className=' text-[#A3A3A3] hover:text-white transition duration-200'>Home</Link>
+                <Link to="/skills" className=' text-[#A3A3A3] hover:text-white transition duration-200'>Skills</Link>
+                <Link to="/education" className=' text-[#A3A3A3] hover:text-white transition duration-200'>Education</Link>
+                <Link to="/projects" className=' text-[#A3A3A3] hover:text-white transition duration-200'>Projects</Link>
+                <Link to="/contact" className=' text-[#A3A3A3] hover:text-white transition duration-200'>Contact</Link>
+            </div>
+            <div className='flex flex-col items-start pl-44 mt-14'>
+                <h4 id="social-section" className="mb-4 text-stone-500">FOLLOW ME</h4>
+                <a 
+                href="https://github.com/harshit-46"
+                className='text-[#A3A3A3] hover:text-white transition duration-200'
+                target="_blank" 
+                rel="noopener noreferrer">
+                    Github
+                </a>
+                <a 
+                href="https://x.com/harshit__25"
+                className='text-[#A3A3A3] hover:text-white transition duration-200'
+                target="_blank" 
+                rel="noopener noreferrer">
+                    X (Twitter)
+                </a>
+                <a 
+                href="https://www.linkedin.com/in/harshithere/"
+                className='text-[#A3A3A3] hover:text-white transition duration-200'
+                target="_blank" 
+                rel="noopener noreferrer">
+                    LinkedIn
+                </a>
+                <a 
+                href="https://www.instagram.com/_harshit.25/"
+                className='text-[#A3A3A3] hover:text-white transition duration-200'
+                target="_blank" 
+                rel="noopener noreferrer">
+                    Instagram
+                </a>
+            </div>
         </div>
     </div>
     )
