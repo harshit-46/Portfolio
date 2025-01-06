f22d5cb891f8f4a1c36aaa2589b622d7dd6495bc
Harshit Gupta <iamharshit999@gmail.com>
2025-01-06 17:13:49 +0530

    Home page updated

--- src/components/test/grid.jsx
+++ src/components/test/grid.jsx
@@ -3,11 +3,11 @@ import '../../assets/CSS/home.css';
 
 export function GridBackgroundDemo() {
     return (
-        <div className="h-[50rem] w-full bg-black bg-grid-white/[0.2] relative flex">
-            <p id="dev-name" className="text-white">
-                Harshit Gupta
-            </p>
-            <p id="dev-para">A passionate individual who always thrives to work on <br></br> end to end products which develop sustainable and <br></br>scalable social and technical systems to create impact.</p>
+        <div className="h-[100vh] w-full bg-black flex">
+            <div className="flex h-fit w-fit mt-28">
+                <h1 className="Home text-white border-2 border-red-600">HARSHIT</h1>
+                <h1 className="homevid ml-4 border-2 border-red-600">GUPTA</h1>
+            </div>
         </div>
     );
 }
