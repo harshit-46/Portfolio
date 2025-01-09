381b87302b546f4de6a8cd48cd7f6a4e1d47277c
Harshit Gupta <iamharshit999@gmail.com>
2025-01-09 11:56:20 +0530

    update

--- src/components/test/grid-tech.jsx
+++ src/components/test/grid-tech.jsx
@@ -2,12 +2,9 @@ import React from "react";
 
 export function GridBackgroundskills() {
     return (
-        <div className="h-[50rem] w-full bg-black bg-grid-white/[0.2] relative flex items-center justify-center">
+        <div className="h-[50rem] w-full bg-black bg-grid-white/[0.2] relative flex -mt-2">
             {/* Radial gradient for the container to give a faded look */}
             <div className="absolute inset-0 flex items-center justify-center pointer-events-none bg-black [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
-            <p className="text-4xl sm:text-7xl font-bold relative z-20 bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-500 py-8">
-                This is Skills.
-            </p>
         </div>
     );
 }
