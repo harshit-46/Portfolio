a4a7101a0f3a9a7c46cbe7b683d42e826de46668
Harshit Gupta <iamharshit999@gmail.com>
2025-01-15 10:27:39 +0530

    update

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,11 +1,9 @@
 import React from 'react';
 import Footer from './Footer';
-import { TracingBeamDemo } from './test/tracebeam';
 
 const Projects = () => {
     return (
         <div className='bg-black'>
-            <TracingBeamDemo/>
             <Footer/>
         </div>
     )
