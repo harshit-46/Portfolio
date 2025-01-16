9eda97b5223dc5fb90c8946031b226806953001d
Harshit Gupta <iamharshit999@gmail.com>
2025-01-16 18:06:47 +0530

    update grid.jsx

--- src/components/test/grid.jsx
+++ src/components/test/grid.jsx
@@ -4,7 +4,7 @@ import '../../assets/CSS/home.css';
 export function HeroSection() {
     return (
         <div className="h-[100vh] w-full bg-black flex">
-            <div className="flex h-fit w-fit mt-24">
+            <div className="flex h-fit mt-16">
                 <h1 className="Home text-white ml-6 text-[190px] leading-none cursor-pointer">HARSHIT</h1>
                 <h1 className="homevid ml-4 text-[190px] leading-none text-transparent bg-cover bg-no-repeat bg-center bg-clip-text bg-gradient-and-gif cursor-pointer">GUPTA</h1>
             </div>
