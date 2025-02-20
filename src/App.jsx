b85d331aafb581d1e9a55ca4e3a745f17ec100f8
Harshit Gupta <iamharshit999@gmail.com>
2025-02-21 00:54:39 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ import Education from "./components/Education";
 
 function App() {
   return (
-    <main className='text-4xl font-bold'>
+    <main>
       <Router>
         <Header/>
         <Routes>
