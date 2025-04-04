2eae5be4c7a9dc41f176f8c5d3c06f23843a67c2
Harshit Gupta <iamharshit999@gmail.com>
2025-04-04 11:30:29 +0530

    update

--- src/components/test/grid-projects.jsx
+++ src/components/test/grid-projects.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export function GridBackgroundProjects() {
     return (
         <div className="h-[50rem] w-full bg-black border-2 border-pink-700">
-            <p className="text-white">Projects</p>
+            <p className="text-white">This is a Projects</p>
         </div>
     );
 }
