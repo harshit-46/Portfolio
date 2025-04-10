f096db2ee441f3586e23c04457e10bc3708721cd
Harshit Gupta <iamharshit999@gmail.com>
2025-04-11 02:01:50 +0530

    update

--- src/assets/icons/images.js
+++
@@ -1,9 +0,0 @@
-const IMAGES = {
-    imgone: require("./ghlogo.png"),
-    imgtwo: require("./newgh.png"),
-    imgthree: require("./ghmain.PNG"),
-    imgfour: require("./AppLogo.png"),
-    imgfive: require("./next.png")
-};
-
-export default IMAGES
