0e6dbb29d6b0fd83bedc4c17d46880cac0e7e472
Harshit Gupta <iamharshit999@gmail.com>
2025-04-11 02:04:03 +0530

    update

--- src/components/Top.jsx
+++ src/components/Top.jsx
@@ -6,7 +6,7 @@ export default function BackToTopBottom() {
 
     return (
         <div className="border-2 border-red-600">
-            <a id="top" onClick={scrollToTop} className="inline-block border-2 border-amber-400">Go Back To Top</a>
+            <a id="top" onClick={scrollToTop} className="inline-block border-2 border-amber-400 cursor-pointer">Go Back To Top</a>
         </div>
     );
 }
