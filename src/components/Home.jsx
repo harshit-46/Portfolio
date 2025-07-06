d2dc32e4eeb91ce84fa82397bb6798ab3c7e0be7
Harshit Gupta <iamharshit999@gmail.com>
2025-07-06 10:56:46 +0530

    update

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -21,7 +21,6 @@ const Home = () => {
                 <span className="h-3 w-3 rounded-full bg-green-500 animate-pulse shadow-[0_0_8px_3px_rgba(34,197,94,0.6)]"></span>
                 <p className="text-white/70 font-semibold">AVAILABLE FOR FREELANCE</p>
             </div>
-
             <div className="font-bold leading-none flex flex-col md:flex-row justify-center gap-x-16 items-center text-center md:text-left">
                 <span className="-ml-0 md:-ml-4 text-white text-[80px] md:text-[120px] lg:text-[190px] tracking-[-0.01em] p-0 m-0 select-none cursor-pointer" id="name1">
                     HARSHIT
@@ -30,15 +29,12 @@ const Home = () => {
                     GUPTA
                 </span>
             </div>
-
             <div className="grid grid-cols-1 md:grid-cols-3 gap-8 items-center justify-items-center px-4 pb-12 md:px-0 md:pb-0 mt-8">
                 <div className="order-1 md:order-none text-white">
                     <a href="/path/to/resume.pdf" target="_blank" rel="noopener noreferrer">
                         <button className="px-4 py-2 border border-white rounded bg-white text-black font-semibold">Resume</button>
                     </a>
-
                 </div>
-
                 <div
                     onClick={handleOnClick}
                     className={`order-2 md:order-none cursor-pointer transform transition duration-500 ${isStraight ? 'rotate-0' : 'rotate-6'
@@ -50,7 +46,6 @@ const Home = () => {
                         className="w-80 h-100 object-cover rounded-2xl border-2 border-white shadow-lg md:-mt-16"
                     />
                 </div>
-
                 <div className="order-3 md:order-none text-white text-center md:text-left max-w-md mt-6 md:mt-0 px-2 md:px-0">
                     <p className="text-lg md:text-xl font-medium leading-relaxed">
                         Hi, I'm <strong>Harshit</strong>, a passionate Full-Stack developer dedicated to creating user-friendly digital experiences.
