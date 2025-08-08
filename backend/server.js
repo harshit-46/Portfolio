723d1197577c128bd98d6a65af12903d7092c187
Harshit Gupta <iamharshit999@gmail.com>
2025-08-09 02:03:59 +0530

    update

--- backend/server.js
+++ backend/server.js
@@ -9,6 +9,9 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+
+console.log('MongoDB URI:', process.env.MONGODB_URI);
+
 mongoose.connect(process.env.MONGODB_URI)
     .then(() => console.log("MongoDB connected"))
     .catch((err) => console.error("MongoDB connection error:", err));
--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -47,7 +47,7 @@ const ContactForm = () => {
         const loadingToast = toast.loading('Sending message...');
 
         try {
-            const res = await fetch('http://localhost:5000/api/contact', {
+            const res = await fetch('https://portfolio-m869.onrender.com/api/contact', {
                 method: 'POST',
                 headers: { 'Content-Type': 'application/json' },
                 body: JSON.stringify(form),
