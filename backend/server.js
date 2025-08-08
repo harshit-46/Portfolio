8aa100c4b85aad40c15bd531afc328a427279aaa
Harshit Gupta <iamharshit999@gmail.com>
2025-08-09 01:23:33 +0530

    update

--- backend/server.js
+++ backend/server.js
@@ -1,26 +1,23 @@
 const express = require('express');
 const cors = require('cors');
-const mongoose = require('mongoose'); 
+const mongoose = require('mongoose');
+require('dotenv').config();
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
 
-// Connect to MongoDB
-mongoose.connect('mongodb://localhost:27017/portfolioDB', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-});
+mongoose.connect(process.env.MONGODB_URI)
+    .then(() => console.log("MongoDB connected"))
+    .catch((err) => console.error("MongoDB connection error:", err));
 mongoose.connection.once('open', () => {
     console.log('Connected to MongoDB');
 });
 
-// Message model
 const Message = require('./models/messages');
 
-// Contact route
 app.post('/api/contact', async (req, res) => {
     const { name, email, message } = req.body;
     try {
